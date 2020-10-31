3b1773def2ac7d0f6b3062e9bf325701a711f04d
Christian Pirhalla <chrispir@udel.edu>
2020-10-31 13:22:03 -0400

    number to display conversion

---
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestDisplayModeConversion.java
@@ -0,0 +1,29 @@
+package com.zipcodewilmington.scientific_calculator;
+
+import com.zipcodewilmington.scientificcalculator.displayModeConversion;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestDisplayModeConversion {
+
+@org.junit.Test
+    public void testConvertToDisplayMode(){
+    displayModeConversion dmc = new displayModeConversion();
+    double testingValue = 200.0;
+
+    // first test: hex
+    //Assert.assertEquals("C8", dmc.convertToDisplayMode("hexadecimal", testingValue));
+
+    // second test: binary
+    Assert.assertEquals(dmc.convertToDisplayMode("binary", testingValue), "11001000");
+
+    // third test:
+    Assert.assertEquals(dmc.convertToDisplayMode("octal", testingValue), "310");
+
+    // fourth test: decimal
+    Assert.assertEquals(dmc.convertToDisplayMode("decimal", testingValue), "200.0");
+    }
+
+}
