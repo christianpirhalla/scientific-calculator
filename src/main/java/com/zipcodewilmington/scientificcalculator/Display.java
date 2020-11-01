55a6d1a9d577aee0b35507aa2c97b4c45b6b9dc3
amanda <amanda@zipcodes-MacBook-Pro-2.local>
2020-11-01 13:44:14 -0500

    Added Display class implementation

--- src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
@@ -2,39 +2,59 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Display {
     private final Console console;
+    private String displayValue;
+    private displayModeConversion conversionMode;
+
     public Display(Console console) {
         this.console = console;
+        this.displayValue = null;
+        this.conversionMode = new displayModeConversion();
     }
+
     public Display() {
         this(new Console());
     }
 
-    public String getDisplayValue() {  // retrieve inputted value
-        return null;
+    public String getDisplayValue() {  // retrieve displayed value
+        return displayValue;
     }
-    public void updateDisplay(String valueToDisplay) { // return the current value presented on the display ??
 
+    public void updateDisplay(String valueToDisplay) { // return the current value presented on the display
+        this.displayValue = valueToDisplay;
     }
 
     public void clearDisplayValue() { // clear the value on the display
-
-    }
-
-    public void updateDisplayValue(String valueToBeUpdated) { // update display with 'valueToBeUpdated'
+        this.displayValue = null;
     }
 
     public void switchDisplayModeToOctal() { // convert displayed value to expression in base eight
+        this.conversionMode.convertToDisplayMode("octal", Double.parseDouble(getDisplayValue()));
     }
 
     public void switchDisplayModeToDecimal() { // convert displayed value to expression in base 10
+        this.conversionMode.convertToDisplayMode("decimal", Double.parseDouble(getDisplayValue()));
     }
 
     public void switchDisplayModeToHexadecimal() { // convert displayed value to expression in base 16
+        this.conversionMode.convertToDisplayMode("hexidecimal", Double.parseDouble(getDisplayValue()));
+    }
+
+    public void switchDisplayTrigonometryMode() {
+        String userInput = console.getStringInput("Would you like to calculate in degrees or radians?");
+        if ("degrees".equalsIgnoreCase(userInput)) {
+            switchDisplayModeToDegrees();
+        } else if ("radians".equalsIgnoreCase(userInput)) {
+            switchDisplayModeToRadians();
+        } else {
+            switchDisplayTrigonometryMode();
+        }
     }
 
     public void switchDisplayModeToDegrees() { //convert displayed value to degrees
+        this.conversionMode.convertToDisplayMode("degree", Double.parseDouble(getDisplayValue()));
     }
 
     public void switchDisplayModeToRadians() { //convert displayed value to radians
+        this.conversionMode.convertToDisplayMode("radians", Double.parseDouble(getDisplayValue()));
     }
 }
